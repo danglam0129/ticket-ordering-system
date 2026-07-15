3f3b34fba651d91ffe086e6d87c5fea6d50356f7
danglam0129 <danglam0129@gmail.com>
2026-07-16 03:53:54 +0800

    Initial commit

---
+++ src/main/java/com/ticket/ordering/system/ticketorderingsystem/TicketOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.ordering.system.ticketorderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketOrderingSystemApplication.class, args);
+	}
+
+}
